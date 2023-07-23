fea2b56a88b5fdc22d6f5c43e8489433acf66c56
Steven Tomlinson <t97steven@gmail.com>
2023-07-23 23:05:47 +0100

    Move order and quiesce search

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,39 +5,85 @@ using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
-    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 20000 };
+    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 0 };
     Board currentBoard;
 
-    int evaluate(int meToMove)
+    int evaluate()
     {
         int sum = 0;
         foreach (PieceType type in Enumerable.Range(1, 6))
             sum += pieceValues[(int)type] * (currentBoard.GetPieceList(type, true).Count - currentBoard.GetPieceList(type, false).Count);
 
-        return sum * meToMove;
+        return sum * (currentBoard.IsWhiteToMove ? 1 : -1);
     }
     int nodes = 0;
 
-    int minimax(int alpha, int beta, int depth, int meToMove, Action<Move> action)
+    void order(Move[] moves)
     {
-        if (depth == 0) return evaluate(meToMove);
+        var move_value = new Dictionary<Move, int>();
+        foreach (Move move in moves)
+        {
+            int score = 0;
+            if (move.IsCapture)
+                score += 10 * pieceValues[(int)move.CapturePieceType] - pieceValues[(int)move.MovePieceType];
+            if (move.IsPromotion)
+                score += pieceValues[(int)move.PromotionPieceType];
+
+            if (currentBoard.SquareIsAttackedByOpponent(move.TargetSquare))
+                score -= pieceValues[(int)move.MovePieceType];
+
+            move_value[move] = score;
+        }
+
+        Array.Sort(moves, (m1, m2) => { return move_value[m2] - move_value[m1]; });
+    }
+
+    int quiesce(int alpha, int beta)
+    {
+        int val = evaluate();
+        if (val >= beta)
+            return beta;
+
+        if (alpha < val)
+            alpha = val;
+
+        var moves = currentBoard.GetLegalMoves(true);
+        order(moves);
+        foreach (Move move in moves)
+        {
+            nodes++;
+            currentBoard.MakeMove(move);
+            val = -quiesce(-beta, -alpha);
+            currentBoard.UndoMove(move);
+            if (val > alpha)
+                alpha = val;
+            if (alpha >= beta) break;
+        }
+        return alpha;
+    }
+
+    int minimax(int alpha, int beta, int depth, Action<Move> action)
+    {
+        if (depth == 0) return quiesce(alpha, beta);
         if (currentBoard.IsDraw()) return 0;
-        if (currentBoard.IsInCheckmate()) return -999999;
+        if (currentBoard.IsInCheckmate()) return -999999 - depth;
 
         var moves = currentBoard.GetLegalMoves();
+        order(moves);
 
         foreach (Move move in moves)
         {
             nodes++;
             currentBoard.MakeMove(move);
-            int val = -minimax(-beta, -alpha, depth - 1, -meToMove, (move) => { });
+            int val = -minimax(-beta, -alpha, depth - 1, (move) => { });
             currentBoard.UndoMove(move);
+
             if (val > alpha)
             {
                 alpha = val;
                 action(move);
             }
-            if (alpha >= beta) return beta;
+            if (alpha >= beta) break;
         }
         return alpha;
     }
@@ -47,7 +93,7 @@ public class MyBot : IChessBot
         currentBoard = board;
         Move retMove = Move.NullMove;
         nodes = 0;
-        var val = minimax(-1073741824, 1073741824, 5, board.IsWhiteToMove ? 1 : -1, (move) => { retMove = move; });
+        var val = minimax(-1073741824, 1073741824, 5, (move) => { retMove = move; });
         Console.WriteLine($"Good Val {val} Searched {nodes}");
         return retMove;
     }
